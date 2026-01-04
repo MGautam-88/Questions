613d4b87a11b094bbc166751e81dffbdc1f93bba
MGautam-88 <162597706+MGautam-88@users.noreply.github.com>
2026-01-04 15:28:12 +0530

    Update MissingNumber-268.cpp

--- MissingNumber-268.cpp
+++ MissingNumber-268.cpp
@@ -3,11 +3,21 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
 
-        ------------------O(n+nlgn)----------------------//
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i!=nums[i]) return i;
+        // ------------------O(n+nlgn)----------------------//
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++){
+        //     if(i!=nums[i]) return i;
+        // }
+        // return n;
+
+        //------------------O(n)-------------------------//
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans^=i;
         }
-        return n;
+        for(int i=0;i<n;i++){
+            ans^=nums[i];//agar 1 se n tak sab honge to x^x = 0 hi hota hai
+        }
+        return ans;
     }
 };
