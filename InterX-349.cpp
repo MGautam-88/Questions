a92df5cf80f63af4220f7fc5e1fba0ad4b03dd95
MGautam-88 <162597706+MGautam-88@users.noreply.github.com>
2026-01-04 19:13:28 +0530

    Update InterX-349.cpp

--- InterX-349.cpp
+++ InterX-349.cpp
@@ -22,3 +22,30 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        int m = b.size();
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        vector<int> ans;
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+            if (a[i] == b[j]) {
+                if ( ans.empty() ||ans.back() != a[i]) {
+                    ans.push_back(a[i]);
+                }
+                i++;
+                j++;
+            } else if (a[i] < b[j]) {
+                i++;
+            } else
+                j++;
+        }
+
+        return ans;
+    }
+};
