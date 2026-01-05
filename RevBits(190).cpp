9e0bfec124ac33544834cb0270b25cc513cd9b11
MGautam-88 <162597706+MGautam-88@users.noreply.github.com>
2026-01-05 17:37:55 +0530

    Create RevBits(190).cpp

---
+++ RevBits(190).cpp
@@ -0,0 +1,28 @@
+// class Solution {
+// public:
+//     string itb(int n) {
+//         string s;
+//         s.reserve(32);
+//         for (int i = 31; i >= 0; i--) {
+//           s.push_back((n & (1 << i)) ? '1' : '0');
+//         }
+//         return s;
+//     }
+
+//     int bti(string s){
+//         int x=0;
+//         for(int i=0;i<32;i++){
+//             if(s[i]=='1'){x += pow(2,31-i);}
+//         }
+//         return x;
+//     }
+
+//     int reverseBits(int n) {
+//         string a=itb(n);
+//         reverse(a.begin(),a.end());
+//         n=bti(a);
+//         return n;
+//     }
+// };
+
+
