513cd51b65f311830bcf0ecaa51c76a16347dfaf
MGautam-88 <162597706+MGautam-88@users.noreply.github.com>
2026-01-12 12:56:14 +0530

    Update Pascal2-119.cpp

--- Pascal2-119.cpp
+++ Pascal2-119.cpp
@@ -4,22 +4,22 @@ public:
         if (rowIndex == 0)return {1};
 
         int n = rowIndex + 1;
-        vector<int> previous;
+        vector<int> prev;
         
         while (n--) {
-            int ps = previous.size();
+            int ps = prev.size();
             vector<int> curr;
             for (int i = 0; i <= ps; i++) {
                 if (i == 0 || i == ps) {
                     curr.push_back(1);
                 } else {
-                    curr.push_back(previous[i - 1] + previous[i]);
+                    curr.push_back(prev[i - 1] + prev[i]);
                 }
             }
 
-            previous = curr;
+            prev = curr;
         }
 
-        return previous;
+        return prev;
     }
 };
