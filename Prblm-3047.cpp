e97c6c824ea0ce4fa9fb9585f1e9053e063eec4f
MGautam-88 <162597706+MGautam-88@users.noreply.github.com>
2026-01-18 03:26:40 +0530

    Refactor largestSquareArea method
    Refactor largestSquareArea function for clarity and efficiency.

--- Prblm-3047.cpp
+++ Prblm-3047.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    long long largestSquareArea(vector<vector<int>>& bottomLeft,
-                                vector<vector<int>>& topRight) {
-        
-        int n = bottomLeft.size();
-        long long ans = 0;
+    long long largestSquareArea(vector<vector<int>>& bl, vector<vector<int>>& tr) {
+        int n= bl.size();
+        int ans=0;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int x=min(tr[i][0],tr[j][0])-max(bl[i][0],bl[j][0]);
+                int y=min(tr[i][1],tr[j][1])-max(bl[i][1],bl[j][1]);
 
-                int left   = max(bottomLeft[i][0], bottomLeft[j][0]);
-                int bottom = max(bottomLeft[i][1], bottomLeft[j][1]);
-
-                int right = min(topRight[i][0], topRight[j][0]);
-                int top   = min(topRight[i][1], topRight[j][1]);
-
-                if(right > left && top > bottom) {
-                    long long side = min(right - left, top - bottom);
-                    ans = max(ans, side * side);
-                }
+                ans=max(ans,min(x,y));
             }
         }
-        return ans;
+
+        return 1LL*ans*ans;
     }
 };
