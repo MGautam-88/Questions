8d9a6ee82e18eddaebe298ee0dba13b921eb574e
MGautam-88 <162597706+MGautam-88@users.noreply.github.com>
2026-01-21 00:02:33 +0530

    Fix loop condition in minBitwiseArray function

--- Prblm-3314.cpp
+++ Prblm-3314.cpp
@@ -4,7 +4,7 @@ public:
         int n=a.size();
         vector<int> b(n,-1);
         for(int i=0;i<n;i++){
-            for(int x=1;x<1001;x++){
+            for(int x=1;x<1000;x++){
                 if((x | (x+1))==a[i]){b[i]=x;break;}
             }
         }
