2654c4630ef0785ede2c77d4206a64ec3149c3e4
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-24 22:52:52 +0530

    Update Prblm-1877.cpp

--- Prblm-1877.cpp
+++ Prblm-1877.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int minPairSum(vector<int>& a) {
         int n= a.size();
-        sort(a.begin(),a.end());
-        vector<int> b;
-        for(int i=0;i<n/2;i++){
-            b.push_back(a[i]+a[n-i-1]);
-        }
         int ans=0;
-        for(int i=0;i<b.size();i++){
-            if(b[i]>ans)ans=b[i];
+        sort(a.begin(),a.end());
+        for(int i=0;i<n/2;i++){
+            ans=max(ans,(a[i]+a[n-i-1]));
         }
 
         return ans;
