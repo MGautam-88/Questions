bb8bd473d28308bcc025b1a0cc388f1478ea9ee6
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-25 01:22:44 +0530

    Update Prblm-1137.cpp

--- Prblm-1137.cpp
+++ Prblm-1137.cpp
@@ -12,3 +12,19 @@ public:
         return p2;
     }
 };
+
+//------------------- SPACE Optimized -------------------
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)return 0;
+        if(n==1||n==2) return 1;
+        int p1=0,p2=1;
+        for(int i=2;i<=n;i++){
+            int t=p2;
+            p2+=p1;
+            p1=t;
+        }
+        return p2;
+    }
+};
