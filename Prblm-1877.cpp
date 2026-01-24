3a35071f0e6c56a59462ef17b0608588444e2c14
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-24 23:18:15 +0530

    Refactor minPairSum to use frequency array

--- Prblm-1877.cpp
+++ Prblm-1877.cpp
@@ -1,3 +1,6 @@
+
+//-----------------------O(n/2 + n*lgn)----------------------------------
+
 class Solution {
 public:
     int minPairSum(vector<int>& a) {
@@ -8,6 +11,33 @@ public:
             ans=max(ans,(a[i]+a[n-i-1]));
         }
 
+        return ans;
+    }
+};
+
+//-----------------------O(2*n)----------------------------------
+
+class Solution {
+public:
+    int minPairSum(vector<int>& a) {
+        int mn=INT_MAX, mx=INT_MIN;
+        vector<int> fq(100001,0);
+        // wahi hai fq ka use kar ke bade chotte ka pair banana hai orr use kar lo to fq--
+        for(int i=0;i<a.size();i++){
+            if(a[i]<mn)mn=a[i];
+            if(a[i]>mx)mx=a[i];
+            fq[a[i]]++;
+        }
+        int ans=0,l=mn,r=mx;
+        while(l<=r){
+            if(!fq[l])l++;// now consider l and r on a simple number line
+            else if(!fq[r])r--;
+            else{
+                ans=max(ans,(l+r));
+                fq[l]--;
+                fq[r]--;
+            }
+        }
         return ans;
     }
 };
