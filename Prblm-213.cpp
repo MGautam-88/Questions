cb3799c8c2cf963d1ba4eadee514574908601946
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-25 17:12:39 +0530

    Optimize space in rob function implementation
    Refactor the rob function for space optimization.

--- Prblm-213.cpp
+++ Prblm-213.cpp
@@ -10,7 +10,6 @@ public:
         inc[0] = a[0];
         inc[1] = max(a[0], a[1]);
 
-        exc[0] = 0;
         exc[1] = a[1];
         exc[2] = max(a[1], a[2]);
 
@@ -19,5 +18,31 @@ public:
             exc[i + 1] = max(a[i + 1] + exc[i - 1], exc[i]);
         }
         return max(inc[n - 2], exc[n - 1]);
+    }
+};
+
+
+//----------------------------SPACE Optimized --------------------------------
+
+class Solution {
+public:
+    int rob(vector<int>& a) {
+        int n = a.size();
+        if (n == 1) return a[0];
+        if (n == 2) return max(a[0], a[1]);
+
+        int i1 = a[0],i2 = max(a[0], a[1]);
+        int e1 = a[1],e2 = max(a[1], a[2]);
+
+        for (int i = 2; i < n - 1; i++) {
+            int t1=i2;
+            i2=max(i2,a[i]+i1);
+            i1=t1;
+
+            int t2=e2;
+            e2=max(e2,a[i+1]+e1);
+            e1=t2;
+        }
+        return max(e2,i2);
     }
 };
