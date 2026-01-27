267567750ec6592fb2c6bcd1a2158308a3aae666
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-27 22:21:39 +0530

    Add space-optimized uniquePathsWithObstacles method
    Implemented a space-optimized solution for unique paths with obstacles using 1D dynamic programming.

--- Prblm-63.cpp
+++ Prblm-63.cpp
@@ -31,3 +31,35 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+
+
+//-------------------------------SPACE Optimized------------------------------------------
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& o) {
+        int m = o.size();
+        int n = o[0].size();
+        if (o[0][0] == 1 || o[m - 1][n - 1] == 1) return 0;
+
+        vector<int> rows(n, 0);
+        rows[0] = 1;
+        for (int j = 1; j < n; j++) {
+            if (!o[0][j - 1])rows[j] = 1;
+            else break;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if ((o[i - 1][j]))rows[j] = 0;
+                if ((o[i][j - 1]))rows[j - 1] = 0;
+                //we are not thinking about 2d dp , we are
+                //handling it using 1D dp so do not worry about in between state of rows 
+                // we need the last m-1 , n-1 th elem only that it!!!!
+                else rows[j] = rows[j] + rows[j - 1];
+            }
+        }
+        return rows[n - 1];
+    }
+};
