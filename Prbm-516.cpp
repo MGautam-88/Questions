2b8cd2f4245fddcb937615b103858ea607192d80
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-29 10:08:31 +0530

    Update Prbm-516.cpp

--- Prbm-516.cpp
+++ Prbm-516.cpp
@@ -29,3 +29,29 @@ public:
         return dp[n-1][n-1];
     }
 };
+
+//--------------------- we can simply play with indexing instead of using a different array(for storing reverse
+
+
+        bool f=0;
+        for(int j=0;j<n;j++){
+            if(a[0]==a[n-j-1])f=1;
+            if(f)dp[0][j]=1;
+        }
+        f=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==a[n-1])f=1;
+            if(f)dp[i][0]=1;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(a[i]==a[n-j-1])dp[i][j]=1+dp[i-1][j-1];
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n-1][n-1];
+//----------------------------------------------------------------------------------------------------------------
