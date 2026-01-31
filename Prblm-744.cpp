d638e6e695696e66e306f2afb96405c669d695b8
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-01-31 16:59:07 +0530

    Add comments for linear search and binary search methods
    Added comments to clarify search methods used in nextGreatestLetter.

--- Prblm-744.cpp
+++ Prblm-744.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& a, char t) {
-
+        // Linear search : O(n)
+        // for(char x:a){if(x>t) return x;}return a[0];
+        
         //BS:O(lg n)
         int l=0,h=a.size()-1;
         int ans=-1;
