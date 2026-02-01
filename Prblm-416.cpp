1629e68d75bd4f68c01fefe0399d284aeebab032
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-01 19:42:24 +0530

    Create Prblm-416.cpp

---
+++ Prblm-416.cpp
@@ -0,0 +1,43 @@
+//--------------------------------------Recursive O(2^n)----------------------------
+class Solution {
+public:
+    bool ck(vector<int>& a, int i, int s) {
+        if (s == 0) return true;
+        if (i >= a.size() || s<0) return 0;
+
+        return ck(a, i + 1, s - a[i]) || ck(a, i + 1, s);
+    }
+
+    bool canPartition(vector<int>& a) {
+        int s = accumulate(a.begin(), a.end(), 0);
+        if (s & 1)return 0;
+
+        return ck(a, 0, s / 2);
+        // we will find the half of sum is possibe or not ,
+        // if yes then the remaing will be automatically s/2 .
+    }
+};
+//----------------------------------------------------------------------------------
+
+//--------------------------SET+DP O(n*s) where s = sum of array--------------------
+class Solution {
+public:
+    bool canPartition(vector<int>& a) {
+        int n=a.size();
+        int s=accumulate(a.begin(),a.end(),0);//as a[i]<100 no need of LL
+        if(s&1)return 0;
+        unordered_set<int> dp;
+        dp.insert(0);
+
+        for(int i=0;i<n;i++){
+            vector<int> prev(dp.begin(), dp.end());
+            for(auto x:prev){
+                dp.insert(x+a[i]);
+                if(x+a[i]==s/2)return 1;
+            }
+        }
+
+        return dp.count(s/2);
+    }
+};
+//-----------------------------------------------------------------------------------
