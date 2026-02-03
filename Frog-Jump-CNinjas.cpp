97137a4db1c8e9d2938182d927dceba8f9264b0b
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-03 19:55:52 +0530

    Update Frog-Jump-CNinjas.cpp

--- Frog-Jump-CNinjas.cpp
+++ Frog-Jump-CNinjas.cpp
@@ -29,3 +29,26 @@ int frogJump(int n, vector<int> &a)
     }
     return p2;
 }
+
+// follow up question instead of 1 or 2 steps we can go anywhere upto k steps 
+class Solution {
+public:
+    int frogJump(vector<int>& a, int k) {
+        int n = a.size();
+        if(n==1)return 0;
+        vector<int> dp(n,0);
+        dp[0]=0;
+        dp[1]=abs(a[0]-a[1]);
+        for(int i=2;i<n;i++){
+            int ans=INT_MAX;
+            for(int j=1;j<=i && j<=k;j++){
+                ans=min(ans,dp[i-j]+abs(a[i]-a[i-j]));
+            }
+            dp[i]=ans;
+        }
+
+        return dp[n-1];
+    }
+};
+// we can SPACE Optimize it to o(k) vector by removing the first elem of the vector and inserting the last one 
+//---------------------------------------------------------------------------------------------------
