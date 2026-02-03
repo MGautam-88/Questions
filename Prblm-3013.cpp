77e033e67d0c09a2ba156c3fb2099e3f5e3500e2
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-03 15:59:22 +0530

    Optimize minimumCost function with heaps
    Refactor solution to use heaps for better performance.

--- Prblm-3013.cpp
+++ Prblm-3013.cpp
@@ -34,5 +34,153 @@ public:
         }
         return ans +a[0];
     }
-};
+};//O(n * d log d)
 //-------------------------------------------------------------------------------------------------------------------
+
+//Now we have to think of some optimall soln , so we will try to solve this issue using *2 MULTI-SETS*
+// MultiSet bcz there are duplicate elem also 
+// 2 bcz if we use only one we have to iteraate over it to get the k-1 elem , which will again increase the complexity
+
+using ll = long long;
+class Solution {
+public:
+    multiset<int> small, large;
+    ll smallSum = 0;
+    int need;
+
+    void rebalance() {
+        while (small.size() > need) {
+            // small mein se last wala bada elem nikalna padega
+            auto i =
+                --small
+                      .end(); // the last elem of the set as end is next to last
+            smallSum -= *i;
+            large.insert(*i);
+            small.erase(i);
+        }
+        while (small.size() < need) {
+            // we will add the starting elem of the large-set
+            auto i = large.begin();
+            smallSum += *i;
+            small.insert(*i);
+            large.erase(i);
+        }
+    }
+
+    void addToSet(int x) {
+        if (small.empty() || x <= *small.rbegin()) {
+            small.insert(x);
+            smallSum += x;
+        } else {
+            large.insert(x);
+        }
+        rebalance();
+    }
+
+    void removeFrmSet(int x) {
+        auto it = small.find(x);
+        if (it != small.end()) {
+            smallSum -= x;
+            small.erase(it);
+        } else {
+            auto it2 = large.find(x);
+            large.erase(it2);
+        }
+        rebalance();
+    }
+
+    long long minimumCost(vector<int>& a, int k, int d) {
+        int n = a.size();
+        need = k - 1;
+
+        // build first window [1 ... 1+d]
+        for (int i = 1; i <= d; i++)
+            addToSet(a[i]);
+
+        ll ans = smallSum;
+
+        for (int i = 2; i < n - d; i++) {
+            removeFrmSet(a[i - 1]);
+            addToSet(a[i + d]);
+            ans = min(ans, smallSum);
+        }
+
+        return ans + a[0];
+    }
+};
+//-------------------But this still got TLE here so we have to use heap for this problem
+
+using ll = long long;
+
+class Solution {
+public:
+    priority_queue<int> small;                 // max-heap
+    priority_queue<int,vector<int>,greater<int>> large; // min-heap
+    unordered_map<int,int> delSmall, delLarge;
+    ll sumSmall = 0;
+    int need;
+
+    void cleanSmall() {
+        while (!small.empty() && delSmall[small.top()]) {
+            delSmall[small.top()]--;
+            small.pop();
+        }
+    }
+    void cleanLarge() {
+        while (!large.empty() && delLarge[large.top()]) {
+            delLarge[large.top()]--;
+            large.pop();
+        }
+    }
+
+    void rebalance() {
+        cleanSmall();
+        cleanLarge();
+
+        while ((int)small.size() > need) {
+            cleanSmall();
+            int x = small.top(); small.pop();
+            sumSmall -= x;
+            large.push(x);
+        }
+        while ((int)small.size() < need) {
+            cleanLarge();
+            int x = large.top(); large.pop();
+            sumSmall += x;
+            small.push(x);
+        }
+    }
+
+    void add(int x) {
+        if (small.empty() || x <= small.top()) {
+            small.push(x);
+            sumSmall += x;
+        } else large.push(x);
+        rebalance();
+    }
+
+    void remove(int x) {
+        if (!small.empty() && x <= small.top()) {
+            delSmall[x]++;
+            sumSmall -= x;
+        } else delLarge[x]++;
+        rebalance();
+    }
+
+    long long minimumCost(vector<int>& a, int k, int d) {
+        need = k - 1;
+        int n = a.size();
+
+        for (int i = 1; i <= d; i++) add(a[i]);
+
+        ll ans = sumSmall;
+
+        for (int i = 2; i + d < n; i++) {
+            remove(a[i-1]);
+            add(a[i+d]);
+            ans = min(ans, sumSmall);
+        }
+        return ans + a[0];
+    }
+};
+
