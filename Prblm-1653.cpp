f041943c3643de0f63c33aac412c39804f297124
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-07 23:06:31 +0530

    Refactor minimumDeletions with space optimization

--- Prblm-1653.cpp
+++ Prblm-1653.cpp
@@ -20,3 +20,40 @@ public:
         return *min_element(ca.begin(),ca.end());
     }
 };
+//--------------------------------------------------Formal DP version---------------------------------------
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        int bCount = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (s[i - 1] == 'b') {
+                dp[i] = dp[i - 1];
+                bCount++;
+            } else {
+                dp[i] = min(dp[i - 1] + 1, bCount);
+            }
+        }
+
+        return dp[n];
+    }
+};
+//------------------------------------------------------------SPACE Optimized-------------------------------------------
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n=s.size();
+        int db=0,ans=0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='b')db++;
+
+            else ans=min(ans+1,db);//ya to a del karo ya b
+        }
+
+        return ans;
+    }
+};
