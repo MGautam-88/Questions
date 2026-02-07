c8faf9d19ed662cada588a1f82cc15fa49adb3b0
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-07 23:04:31 +0530

    Update Prblm-1653.cpp

--- Prblm-1653.cpp
+++ Prblm-1653.cpp
@@ -1,21 +1,22 @@
- int remainingA = 0;
-        for (char c : s) {
-            if (c == 'a') remainingA++;
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n=s.size();
+        vector<int> ca(n,0),cb(n,0);
+        for(int i=1;i<n;i++){
+            if(s[i-1]=='b')cb[i]=cb[i-1]+1;
+            else cb[i]=cb[i-1];
         }
 
-        int bCount = 0;
-        int ans = remainingA;
-
-        for (char c : s) {
-            if (c == 'a') {
-                remainingA--;
-            }
-
-            ans = min(ans, bCount + remainingA);
-
-            if (c == 'b') {
-                bCount++;
-            }
+        for(int i=n-2;i>=0;i--){
+            if(s[i+1]=='a')ca[i]=ca[i+1]+1;
+            else ca[i]=ca[i+1];
         }
 
-        return ans;
+        for(int i=0;i<n;i++){
+            ca[i]+=cb[i];
+        }
+
+        return *min_element(ca.begin(),ca.end());
+    }
+};
