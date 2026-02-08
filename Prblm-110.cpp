d207e4dbbb83a43accce3b844abcb3e498016e3c
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-08 10:17:19 +0530

    Optimize isBalanced function for tree height calculation
    Refactor isBalanced function to calculate height and balance in a single iteration, improving efficiency.

--- Prblm-110.cpp
+++ Prblm-110.cpp
@@ -17,6 +17,31 @@ public:
 
         return isBalanced(x->left) && isBalanced(x->right);
         //pehle pura left wala subtree check hoga fir right wala subtree
+        //or isme har node ke liye height calc kar rahe hein to complexity jyada ho rahi hai 
     }
 };
 //---------------------------------------------------------------------------------------------------------------------------------
+//------------------------we will check if balanced in the same iteration as in calc height =>O(n)---------------------------------
+
+class Solution {
+public:
+    int cH(TreeNode* x){
+        if(x==nullptr)return 0;
+
+        int lh=cH(x->left);
+        if(lh==-1)return -1;//early return
+
+        int rh=cH(x->right);
+        if(rh==-1)return -1;//early return 
+
+        if(abs(lh-rh)>1)return -1;
+
+        return max(lh,rh)+1;
+    }
+
+    bool isBalanced(TreeNode* x) {
+        return cH(x)!=-1;
+        //ch mein hi check ho ja raha hai saath saath =>O(n)
+        //where n is the number of nodes
+    }
+};
