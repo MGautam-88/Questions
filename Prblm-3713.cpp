d94d21609dce147c8a117d44ea34cf911318a9ca
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-12 18:06:06 +0530

    Create Prblm-3713.cpp

---
+++ Prblm-3713.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n=s.size();
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            if(n-i<=ans)break;//as ans can't be find in this smaller window further
+            map<char,int> fr; 
+            for(int j=i;j<n;j++){
+                fr[s[j]]++;
+                bool f=1;
+
+                if (!fr.empty()) {
+                    auto it = fr.begin();
+                    int val = it->second;
+                    ++it;
+
+                    for (; it != fr.end(); ++it) {
+                        if (it->second != val) {
+                            f = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (f) ans = max(ans, j - i + 1);
+                if(ans==n)return n;
+            }
+        }
+
+        return ans;
+    }
+};//O(n^3)
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
