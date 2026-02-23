ec9ebd42bb9145c407c8748c4a8ec8e73e8ad83a
Gautam Mallhotra <162597706+MGautam-88@users.noreply.github.com>
2026-02-23 18:40:48 +0530

    Update Prblm-1461.cpp

--- Prblm-1461.cpp
+++ Prblm-1461.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
+        unordered_set<string> c2;
+
+        int l=0,r=k-1;
+        while(r<s.size()){
+            string t(s.begin()+l,s.begin()+r+1);//o(k)
+            c2.insert(t);
+            l++;
+            r++;
+        }
+
+        if(c2.size()<pow(2,k))return false;
+        return true;
+    }
+};// O(nk + 2^k)
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
+//Single contiguous block of memory ,No hashing ,No dynamic node allocation ,Excellent cache locality
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
         vector<bool> seen(pow(2,k),0);
 
         int l=0,r=k-1;
